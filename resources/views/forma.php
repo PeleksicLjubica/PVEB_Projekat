843be0b6d7ea7c755d980f423df61cb81a3c6669
Milica Kojicic <milicakojicic@gmail.com>
2016-05-10 22:28:07 +0200

    Postavljanje arhitekture projekta uz Lumen framework

---
+++ resources/views/forma.php
@@ -0,0 +1,336 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Demo</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script type="text/javascript" src="https://gc.kis.scr.kaspersky-labs.com/1B74BD89-2A22-4B93-B451-1C9E1052A0EC/main.js" charset="UTF-8"></script><script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+
+<div class="container-fluid" id="content">
+
+    <?php
+        include 'header.php';
+    ?>
+
+
+    <div class="container" align="center">
+        <h2>Karton filma</h2>
+
+        <div class="col-md-2"></div>
+        <div class="col-md-8">
+        <form class="form-horizontal" role="form">
+            <div id="forma1">
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="godina">Godina proizvodnje:</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="godina">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="katedra">Katedra:</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="katedra">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="naziv">Naziv filma:</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="naziv">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="naziv_vezbe">Naziv vežbe:</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="naziv_vezbe">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="predmet">Predmet:</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="predmet">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="profesor">Profesor:</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="profesor">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="godina_studija">Godina studija:</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="godina_studija">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="trajanje">Trajanje:</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="trajanje">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="reziser">Režiser:</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="reziser">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="producent">Producent:</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="producent">
+                    </div>
+                </div>
+
+
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="scenarista">Scenarista:</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="scenarista">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="snimatelj">Snimatelj:</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="snimatelj">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="montazer">Montažer:</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="montazer">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class = "form-horizontal">
+                        <label class="control-label col-sm-2" for="glumci">Glumci:</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" id="glumci">
+                        </div>
+                        <div class="col-sm-1">
+                            <button type="button" class="btn btn-default">+</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="dizajner_zvuka">Dizajner zvuka:</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="dizajner_zvuka">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="snimatelj_zvuka">Snimatelj zvuka:</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="snimatelj_zvuka">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="kompozitor">Kompozitor:</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="kompozitor">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="scenograf">Scenograf:</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="scenograf">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="kostimograf">Kostimograf:</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="kostimograf">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="specijalni_efekti">Specijalni efekti:</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="specijalni_efekti">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="animacija">Animacija:</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="animacija">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="sminker">Sminker:</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="sminker">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="arhivska_muzika">Arhivska muzika:</label>
+                    <div class="col-sm-9">
+                        <textarea rows="5" cols="5" class="form-control" id="arhivska_muzika"></textarea>
+                    </div>
+
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="sinopsis">Sinopsis:</label>
+                    <div class="col-sm-9">
+                        <textarea rows="7" cols="5" class="form-control" id="sinopsis"></textarea>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="bio_rezisera">Kratka biografija rezisera:</label>
+                    <div class="col-sm-9">
+                        <textarea rows="7" cols="5" class="form-control" id="bio_rezisera"></textarea>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class = "form-horizontal">
+                        <label class="control-label col-sm-2" for="nagrade">Nagrade:</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" id="nagrade"></textarea>
+                        </div>
+                        <div class="col-sm-1">
+                            <button type="button" class="btn btn-default">+</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-sm-2" for="napomene">Napomene:</label>
+                    <div class="col-sm-9">
+                        <textarea rows="7" cols="5" class="form-control" id="napomene"></textarea>
+                    </div>
+                </div>
+            </div>
+
+            <div id="forma2">
+                <h3>Tehnička specifikacija: </h3>
+
+                <div class="form-group">
+                    <label class="control-label col-md-2" for="osnovni_format">Osnovni format</label>
+                    <div class="radio-inline">
+                        <label><input type="radio" name="osnovni_format" id="osnovni_format">VIDEO</label>
+                    </div>
+                    <div class="radio-inline">
+                        <label><input type="radio" name="osnovni_format">FILM</label>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-md-2" for="filmski_format">Filmski format</label>
+                    <div class="radio-inline">
+                        <label><input type="radio" name="filmski_format" id="filmski_format">8 mm</label>
+                    </div>
+                    <div class="radio-inline">
+                        <label><input type="radio" name="filmski_format">16 mm</label>
+                    </div>
+                    <div class="radio-inline">
+                        <label><input type="radio" name="filmski_format">35 mm</label>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-md-2" for="video_format">Filmski format</label>
+                    <div class="radio-inline">
+                        <label><input type="radio" name="video_format" id="video_format">SD</label>
+                    </div>
+                    <div class="radio-inline">
+                        <label><input type="radio" name="video_format">HD</label>
+                    </div>
+                    <div class="radio-inline">
+                        <label><input type="radio" name="video_format">2K</label>
+                    </div>
+                    <div class="radio-inline">
+                        <label><input type="radio" name="video_format">4K</label>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-md-2" for="tel_standard">Televizijski standard</label>
+                    <div class="radio-inline">
+                        <label><input type="radio" name="tel_standard" id="tel_standard">PAL</label>
+                    </div>
+                    <div class="radio-inline">
+                        <label><input type="radio" name="tel_standard">NTSC</label>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-md-2" for="an_slike">Analiza slike</label>
+                    <div class="radio-inline">
+                        <label><input type="radio" name="an_slike" id="an_slike">Sa proredom</label>
+                    </div>
+                    <div class="radio-inline">
+                        <label><input type="radio" name="an_slike">Progresivna</label>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-md-2" for="format_slicice">Format slicice</label>
+                    <div class="radio-inline">
+                        <label><input type="radio" name="format_slicice" id="format_slicice">4:3</label>
+                    </div>
+                    <div class="radio-inline">
+                        <label><input type="radio" name="format_slicice">4:3 Letterbox</label>
+                    </div>
+                    <div class="radio-inline">
+                        <label><input type="radio" name="format_slicice">16:9</label>
+                    </div>
+                    <div class="radio-inline">
+                        <label><input type="radio" name="format_slicice">16:9 Letterbox</label>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-7">
+                    <button type="submit" class="btn btn-default">Potvrdi</button>
+                </div>
+            </div>
+
+
+        </form>
+        </div>
+    <div class="col-md-2"></div>
+    </div>
+
+
+    <?php
+        include 'footer.php';
+    ?>
+
+</body>
+</html>
