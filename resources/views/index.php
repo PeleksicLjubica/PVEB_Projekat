5540a43fdba7a2b055889fc0b870334f1c0a0e8d
LjubicaPeleksic <peleksic.ljubica@gmail.com>
2016-05-12 18:40:26 +0200

    Sitne promene i prebacivanje bootstrap-a u public modela

--- resources/views/index.php
+++ resources/views/index.php
@@ -3,57 +3,57 @@
         include 'header.php';
     ?>
 
-    <div class="row" id="navigation">
-        <div class="col-md-8" id="naslov">
-            <h1>Pretraga fimova:</h1>
-        </div>
-    </div><!-- div navigacija -->
+   <div class="container">
 
-    <div class="row">
-        <div class="col-md-12" id="forma">
-            <form class="form-horizontal" role="form">
-                <div class="form-group">
-                    <label class="col-md-1" for="naziv_filma">Naziv filma: </label>
-                    <div class="col-md-4">
-                        <input type="text" class="form-control" name="naziv_filma" id="naziv_filma">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-md-1" for="naziv_vezbe">Naziv vezbe: </label>
-                    <div class="col-md-4">
-                        <input type="text" class="form-control" name="naziv_vezbe" id="naziv_vezbe"><br>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-md-1" for="predmet">Predmet: </label>
-                    <div class="col-md-4">
-                        <input type="text" class="form-control" name="predmet" id="predmet"/>
-                    </div>
-                </div>
+        <div class="row" id="navigation">
+            <div class="col-md-8" id="naslov">
+                <h1>Pretraga fimova:</h1>
+            </div>
+        </div><!-- div navigacija -->
 
-                <div class="form-group">
-                    <label class="col-md-1" for="sel1">Profesor:</label>
-                    <div class="col-md-2">
-                        <select class="form-control" id="sel1">
-                            <option>A</option>
-                            <option>B</option>
-                            <option>C</option>
-                            <option>D</option>
-                        </select>
+        <div class="row">
+            <div class="col-md-12" id="forma">
+                <form class="form-horizontal" role="form">
+                    <div class="form-group">
+                        <label class="col-md-1" for="naziv_filma">Naziv filma: </label>
+                        <div class="col-md-4">
+                            <input type="text" class="form-control" name="naziv_filma" id="naziv_filma">
+                        </div>
                     </div>
-                </div>
+                    <div class="form-group">
+                        <label class="col-md-1" for="naziv_vezbe">Naziv vezbe: </label>
+                        <div class="col-md-4">
+                            <input type="text" class="form-control" name="naziv_vezbe" id="naziv_vezbe"><br>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-1" for="predmet">Predmet: </label>
+                        <div class="col-md-4">
+                            <input type="text" class="form-control" name="predmet" id="predmet"/>
+                        </div>
+                    </div>
 
-                <button type="button" class="btn btn-default" id="pretrazi">Pretrazi</button>
+                    <div class="form-group">
+                        <label class="col-md-1" for="sel1">Profesor:</label>
+                        <div class="col-md-2">
+                            <select class="form-control" id="sel1">
+                                <option>A</option>
+                                <option>B</option>
+                                <option>C</option>
+                                <option>D</option>
+                            </select>
+                        </div>
+                    </div>
 
-            </form>
-        </div>
-    </div><!-- div red-->
+                    <button type="button" class="btn btn-default" id="pretrazi">Pretrazi</button>
+
+                </form>
+            </div>
+        </div><!-- div red-->
+
+   </div>
 
     <?php
         include 'footer.php';
     ?>
 
-</div>
-
-</body>
-</html>
