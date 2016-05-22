6fb568d92b9b2bc34bd419063cc2085e6be28bff
LjubicaPeleksic <peleksic.ljubica@gmail.com>
2016-05-22 18:58:09 +0200

    Probni fajlovi Angular-a

---
+++ app/Models/Film.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+final class Film extends Model
+{
+    protected $table='Film';
+
+
+}
+
---
+++ public/ts/forma.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by Ljubica on 22.5.2016.
+ */
+import {bootstrap} from "../node_modules/angular2/platform/browser";
+import {Component} from "../node_modules/angular2/core";
+
+@Component({
+    selector: 'app',
+    template: '<div>Cao svete</div>'
+})
+
+class App{}
+
+bootstrap(App);
+
