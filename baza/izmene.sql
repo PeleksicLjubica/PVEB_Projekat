0b52a8ff704668fd4423ff015e0935c90dc4486c
ivan-milic <milic92@gmail.com>
2016-05-24 19:22:06 +0200

    Premestena skripta baze i dodat fajl za izmene u bazi

---
+++ baza/izmene.sql
@@ -0,0 +1,2 @@
+INSERT INTO `mydb`.`profesor` (`ime`, `prezime`) VALUES ('Petar', 'Radanovic');
+INSERT INTO `mydb`.`katedra` (`godina_studija`, `skolska_godina`, `naziv`, `Profesor_id_profesora`) VALUES ('2016', '1', 'Montaza', '1');
