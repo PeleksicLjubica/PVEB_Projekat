efdc5cdb7978764423613a8e1287af7a81a27952
LjubicaPeleksic <peleksic.ljubica@gmail.com>
2016-06-13 01:35:38 +0200

    Sredjivanje forme za prikaz pojedinacnog filma i za unos studenta

--- resources/views/film.php
+++ resources/views/film.php
@@ -5,76 +5,165 @@ include 'header.php';
 
 <div class="container">
 
-    <div class="row">
-        <div class="col-md-1"></div>
-        <div class="col-md-10">
+    <div class="row" id="proba">
+        <div class="col-md-4"><img src="img/movie.jpg" width="300px" height="300px"></div>
+
+        <div class="col-md-4">
+            <label id="naziv_filma"> OVO JE NAZIV </label>
+            <p>Godina proizvodnje: <label id="godina_proizvodnje"> 2006 </label> </p>
+            <p>Trajanje: <label id="trajanje"> 12:12:12 </label> </p>
+            <p>Katedra: <label id="katedra"> rezija </label> </p>
+            <p>Profesor: <label id="profesor"> Andjelka </label> </p>
+            <p>Godina studija: <label id="godina_studija"> 2 </label></p>
+            <p>Naziv vezbe: <label id="naziv_vezbe"> vezba1 </label> </p>
+            <p>Predmet: <label id="predmet"> bla </label> </p>
+
+        </div>
+
+
+
+        <div class="col-md-4">
+            <div class="row">
+                <div class="col-md-12">
+                    <button type="button" id="oi" class="btn btn-default btn-block" data-toggle="collapse" data-target="#osn_informacije">
+                       Detalji
+                    </button>
+                </div>
+            </div>
+
 
             <div class="row">
-
-                <h2>Naziv filma: <label id="naziv_filma"> OVO JE NAZIV </label></h2>
-
-                <div id="napredna_pretraga">
-                    Informacije: <span class="glyphicon glyphicon-triangle-bottom glyphicon"></span>
+                <div class="col-md-12">
+                    <button type="button" id="pusti_film" class="btn btn-default btn-block" data-toggle="collapse" data-target="#prikaz_filma">
+                        Prikaz filma
+                    </button>
                 </div>
+            </div>
 
-                <div id="opcije">
-                    <div id="osnovne_informacije" class="col-md-12">
-                        <p class="pasusi" id="pasus1"> Osnovne informacije:
-                            <span class="glyphicon glyphicon-triangle-bottom glyphicon"></span>
-                        </p>
 
-                        <div class="row" id="osnovno">
+        </div>
+    </div>
 
-                            <p>Godina proizvodnje: <label id="godina_proizvodnje"> 2006 </label> </p>
-                            <p>Katedra: <label id="katedra"> rezija </label> </p>
-                            <p>Naziv vezbe: <label id="naziv_vezbe"> 2006 </label> </p>
-                            <p>Godina studija: <label id="godina_studija"> 2006 </label> </p>
-                            <p>Profesor: <label id="profesor"> 2006 </label> </p>
-                            <p>Predmet: <label id="predmet"> 2006 </label> </p>
-                            <p>Trajanje: <label id="trajanje"> 2006 </label> </p>
-                            <p>Režiser: <label id="reziser"> 2006 </label> </p>
-                            <p>Producent: <label id="producent"> 2006 </label> </p>
-                            <p>Scenarista: <label id="scenarista"> 2006 </label> </p>
-                            <p>Snimatelj: <label id="snimatelj"> 2006 </label> </p>
-                            <p>Montažer: <label id="montazer"> 2006 </label> </p>
-                            <p>Glumac: <label id="glumac"> 2006 </label> </p>
-                            <p>Kompozitor: <label id="kompozitor"> 2006 </label> </p>
-                            <p>Dizajner zvuka: <label id="dizajner_zvuka"> 2006 </label> </p>
-                            <p>Godina proizvodnje: <label id="snima"> 2006 </label> </p>
+    <div class="row">
 
-                            Dizajner zvuka:
-                            Snimatelj zvuka:
-                            Scenograf:
-                            Kostimograf:
-                            Animacija:
-                            Šminker:
-                            Specijalni efekti:
+        <div class="row" id="opcije">
+
+            <div class="col-md-6">
+            <div class="collapse" id="osn_informacije">
+                <h3 id="oi_naslov"> Osnovne informacije:</h3>
+                        <div class="row">
+
+                            <div class="col-md-12">
+
+                                <table class="table table-striped">
+                                    <tbody>
+                                    <tr>
+                                        <td>Režiser:</td>
+                                        <td>Mika</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Producent</td>
+                                        <td>Pera</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Scenarista</td>
+                                        <td>Mika</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Snimatelj</td>
+                                        <td>Mika</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Monatažer</td>
+                                        <td>Mika</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Glumac</td>
+                                        <td>Mika</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Kompozitor</td>
+                                        <td>Mika</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Dizajener zvuka</td>
+                                        <td>Mika</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Snmatelj zvuka</td>
+                                        <td>Mika</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Scenograf</td>
+                                        <td>Mika</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Kostimograf</td>
+                                        <td>Mika</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Animacija</td>
+                                        <td>Mika</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Sminker</td>
+                                        <td>Mika</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Specijalni efekti</td>
+                                        <td>Mika</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+
+
+                                <h3 id="ts_naslov"> Tehnicka specifikacija:</h3>
+
+                                <div class="row" >
+                                    <div class="col-md-6">
+                                        <p>Osnovni format: <label id="osnovni_format"> VIDEO </label> </p>
+                                        <p>Video format: <label id="video_format"> rezija </label> </p>
+                                        <p>Analiza slike: <label id="analiza_slike"> rezija </label> </p>
+                                        <p>Format sličice: <label id="format_slicice"> rezija </label> </p>
+                                        <p>Katedra: <label id="katedra"> rezija </label> </p>
+                                        <p>Video nosač: <label id="video_nosac"> rezija </label> </p>
+                                        <p>Zvuk: <label id="zvuk"> rezija </label> </p>
+                                    </div>
+
+                                    <div class="col-md-6">
+                                        <p>Filmski format: <label id="filmski_format"> rezija </label> </p>
+                                        <p>Televizijski standard: <label id="televizijski_standard"> rezija </label> </p>
+                                        <p>Sličiceu sekundi: <label id="slicice_u_sekundi"> rezija </label> </p>
+                                        <p>Vrsta fajla: <label id="vrsta_fajla"> rezija </label> </p>
+                                        <p>Broj kanala: <label id="broj_kanala"> rezija </label> </p>
+                                        <p>Redukcija šuma: <label id="redukcija_suma"> rezija </label> </p>
+
+                                    </div>
+                                </div>
+
+                            </div>
+
 
                         </div>
-
-                    </div>
-                    <div id="tehnicka_specifikacija" class="col-md-12">
-                        <p class="pasusi" id="pasus2"> Tehnicka specifikacija:
-                            <span class="glyphicon glyphicon-triangle-bottom glyphicon"></span>
-                        </p>
-
-                        <div class="row" id="tehnicka">
-                            <p> BLA BLA BLA </p>
-                        </div>
-                    </div>
-                </div>
+            </div>
+            </div>
 
 
+            <div class="col-md-6 collapse" id="prikaz_filma">
+                <video id="video1" width="600" controls>
+                    <source src="filmovi/TOM.mp4" type="video/mp4">
+                    Your browser does not support HTML5 video.
+                </video>
             </div>
 
         </div>
-        <div class="col-md-1"></div>
-    </div>
 
+
+    </div>
 
 </div>
 
-<script src="js/film.js"></script>
+
 
 
 <?php
--- resources/views/studenti.php
+++ resources/views/studenti.php
@@ -30,14 +30,19 @@ include 'header.php';
 
                         <div id="studenti_forma">
                             <form role="form" method="post" enctype="multipart/form-data" action="#" onsubmit="pathWithToken('student')">
-                                <div class="form-group">
-                                    <label for="indeks">Indeks studenta:</label>
-                                    <input type="text" class="form-control" id="indeks" name="indeks">
+                                <div class="row">
+                                    <div class="form-group col-md-12">
+                                        <label for="indeks">Indeks studenta:</label>
+                                        <input type="text" class="form-control" id="indeks" name="indeks">
+                                    </div>
                                 </div>
-                                <div class="form-group">
-                                    <label for="ime">Ime:</label>
-                                    <input type="text" class="form-control" id="ime" name="ime">
+                                <div class="row">
+                                    <div class="form-group col-md-12">
+                                        <label for="ime">Ime:</label>
+                                        <input type="text" class="form-control" id="ime" name="ime">
+                                    </div>
                                 </div>
+                                
                                 <div class="form-group">
                                     <label for="indeks">Prezime:</label>
                                     <input type="text" class="form-control" id="prezime" name="prezime">
@@ -47,17 +52,15 @@ include 'header.php';
                                     <input type="text" class="form-control" id="email" name="email">
                                 </div>
 
-                                <div class="form-group">
+                                <div class="form-group" >
                                     <label for="godina_studija"> Godina studija: </label>
-                                    <div class="col-md-9">
-                                        <select class="js-example-data-array col-md-12 godina_studija" name="godina_studija">
-                                        </select>
-                                    </div>
+                                    <select class="js-example-data-array col-md-12 godina_studija" name="godina_studija">
+                                    </select>
                                 </div>
 
                                 <div class="form-group">
                                     <label>Katedra na kojoj je student:</label>
-                                    <select class="js-example-data-array katedre" name="katedra" style="width: 100%">
+                                    <select class="js-example-data-array katedre"  name="katedra" style="width: 100%">
                                     </select>
                                 </div>
 
@@ -67,20 +70,7 @@ include 'header.php';
                         </div>
 
                     </div> <!--kraj drugog diva-->
-                    <div class="col-md-3">
-                        <div id="inkrement_godine" class="pasusi">
-                            Uvećaj godinu studentu <span class="glyphicon glyphicon-triangle-bottom glyphicon"></span>
-                        </div>
-                        <div id="studenti_forma1">
-                            <form role="form" method="post" enctype="multipart/form-data" action="#" onsubmit="pathWithToken('student')">
-                                <div class="form-group">
-                                    <label for="indeks">Indeks studenta:</label>
-                                    <input type="text" class="form-control" id="indeks" name="indeks">
-                                </div>
-                                <button type="submit" class="btn btn-default" id="dugme_studenti1">Uvecaj</button>
-                            </form>
-                        </div>
-                    </div>
+
                 </div>
             </div>
         </section>
